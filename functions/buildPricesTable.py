98233e556e4aa19ff7de99016fedce59f996424a
fabimass <massotto.fabian@gmail.com>
2023-01-13 21:12:23 -0300

    Getting prices from yahoo finance

---
+++ functions/buildPricesTable.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import yfinance as yfin
+from pandas_datareader import data as pdr
+
+def buildPricesTable(tokens, date_start, date_end):
+    # Prepare dataframe for the prices
+    prices_df = pd.DataFrame(columns = ["Date", "High", "Low", "Open", "Close", "Token"])
+
+    # This overrides pandas data reader in order to fix some current issues with the library
+    yfin.pdr_override()
+
+    # Loop through each of the tokens getting the data from Yahoo Finance
+    for token in tokens:
+        print(f"Getting prices for {token}...")
+        symbol = token + "-USD"
+        temp_df = pdr.get_data_yahoo(symbol, start=date_start, end=date_end)
+        temp_df = temp_df.reset_index(level=0)
+        temp_df["Token"] = token
+        del temp_df["Volume"]
+        del temp_df["Adj Close"]
+        prices_df = pd.concat([prices_df, temp_df])
+        print("OK")
+
+    return prices_df
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sys
 from functions.getParams import getParams
+from functions.buildPricesTable import buildPricesTable
 
 if __name__ == "__main__":
     # Parse script parameters
     start_date, end_date, assets_list, input_file = getParams(sys.argv)
 
-    print('From:', start_date)
-    print('To:', end_date)
-    print('Assets:', assets_list)
-    print('Input:', input_file)
+    # Create prices table
+    prices = buildPricesTable(assets_list, start_date, end_date)
+    print(prices)
 
 
     print("Script executed successfully!!\n Enter any key to exit")
