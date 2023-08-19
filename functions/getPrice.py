82f66fce06ee4aaa6d103dfb7934292c180cdc94
fabimass <massotto.fabian@gmail.com>
2023-08-19 16:13:17 -0300

    getting prices for different suffixes

--- functions/getPrice.py
+++ functions/getPrice.py
@@ -2,40 +2,46 @@ import pandas as pd
 import yfinance as yfin
 from pandas_datareader import data as pdr
 
-def getPrice(tokens, ignore, corrector, date_start, date_end, suffix):
+def getPrice(tokens, ignore, corrector, date_start, date_end, suffixes):
     # Prepare dataframe for the prices
-    prices_df = pd.DataFrame(columns = ["Date", "High", "Low", "Open", "Close", "Token"])
+    prices_df = pd.DataFrame(columns = ["Date", "High", "Low", "Open", "Close", "Token", "Suffix"])
 
     # This overrides pandas data reader in order to fix some current issues with the library
     yfin.pdr_override()
 
-    # Loop through each of the tokens getting the data from Yahoo Finance
+    # Prepair the list of symbols
+    symbols = []
     for token in tokens:
         if token in ignore:
-            print(f"Ignoring prices for {token}...")
+            print(f"{token} prices will be ignored...")
             pass
         else:
-            print(f"Getting prices for {token}...")
-            try:
-                if suffix:
-                    symbol = token + suffix
-                else:
-                    symbol = token
-                temp_df = pdr.get_data_yahoo(symbol, start=date_start, end=date_end)
-                temp_df = temp_df.reset_index(level=0)
-                temp_df["Token"] = token
-                del temp_df["Volume"]
-                del temp_df["Adj Close"]
-                print(temp_df)
-                if token in corrector:
-                    print(f"Price will be multiplied by {corrector[token]}...")
-                    temp_df['High'] = temp_df['High'].apply(lambda x: x * float(corrector[token]) if pd.notnull(x) else x)
-                    temp_df['Low'] = temp_df['Low'].apply(lambda x: x * float(corrector[token]) if pd.notnull(x) else x)
-                    temp_df['Open'] = temp_df['Open'].apply(lambda x: x * float(corrector[token]) if pd.notnull(x) else x)
-                    temp_df['Close'] = temp_df['Close'].apply(lambda x: x * float(corrector[token]) if pd.notnull(x) else x)
-                prices_df = pd.concat([prices_df, temp_df])
-                print("OK")
-            except:
-                print(f"ERROR: Something went wrong...")
+            if suffixes:
+                for suffix in suffixes:
+                    symbols.append({"symbol": f"{token}{suffix}", "token": token, "suffix": suffix})
+            else:
+                symbols.append({"symbol": token, "token": token, "suffix": None})
+
+    # Loop through each of the symbols getting the data from Yahoo Finance
+    for symbol in symbols:  
+        print(f"Getting prices for {symbol['symbol']}...")
+        try:
+            temp_df = pdr.get_data_yahoo(symbol['symbol'], start=date_start, end=date_end)
+            temp_df = temp_df.reset_index(level=0)
+            temp_df["Token"] = symbol['token']
+            temp_df["Suffix"] = symbol['suffix']
+            del temp_df["Volume"]
+            del temp_df["Adj Close"]
+            print(temp_df)
+            if token in corrector:
+                print(f"Price will be multiplied by {corrector[token]}...")
+                temp_df['High'] = temp_df['High'].apply(lambda x: x * float(corrector[token]) if pd.notnull(x) else x)
+                temp_df['Low'] = temp_df['Low'].apply(lambda x: x * float(corrector[token]) if pd.notnull(x) else x)
+                temp_df['Open'] = temp_df['Open'].apply(lambda x: x * float(corrector[token]) if pd.notnull(x) else x)
+                temp_df['Close'] = temp_df['Close'].apply(lambda x: x * float(corrector[token]) if pd.notnull(x) else x)
+            prices_df = pd.concat([prices_df, temp_df])
+            print("OK")
+        except:
+            print(f"ERROR: Something went wrong...")
 
     return prices_df
